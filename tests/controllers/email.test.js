8d6e5aa86dd125e773f1bd545f85d8771e76f621
Daniel Espinosa <d111092.de@gmail.com>
2023-12-11 17:49:41 -0600

    test: update test description style: remove comments on sql file

--- tests/controllers/email.test.js
+++ tests/controllers/email.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const expect = require('chai').expect;
 
 describe('email', () => {
-    it('should do smthg', (done) => {
+    it('should verify email', (done) => {
         request('http://localhost:8080')
             .get('/api/email/verifyEmail')
             .query({ email: 'daniel@mail.com' })
@@ -10,8 +10,6 @@ describe('email', () => {
 			.set('Content-Type', 'application/json')
 			.end(function (err, res) {
 				
-                console.log(res)
-
                 expect(res.statusCode).to.be.equal(200);
 				expect(res.body.msg).to.be.equal('email verified');
 				expect(res.body.email).to.be.equal('daniel@mail.com');
