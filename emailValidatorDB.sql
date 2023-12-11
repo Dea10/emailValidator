82cea475622ce10792388859f509bfba521c7f78
Daniel Espinosa <d111092.de@gmail.com>
2023-12-11 17:45:40 -0600

    test: rename test

--- emailValidatorDB.sql
+++ emailValidatorDB.sql
@@ -35,3 +35,18 @@ CREATE TABLE phone_user (
     FOREIGN KEY (user_id) REFERENCES user(id),
     FOREIGN KEY (phone_id) REFERENCES phone(id)
 );
+
+-- JOINS
+-- SELECT name, email, phone 
+-- FROM user 
+-- LEFT JOIN email_user ON user.id = email_user.user_id
+-- LEFT JOIN email ON email_user.email_id = email.id
+-- LEFT JOIN phone_user ON user.id = phone_user.user_id
+-- LEFT JOIN phone ON phone_user.phone_id = phone.id
+-- WHERE email = 'arturo@mail.com';
+
+-- INSERT INTO email (email) VALUES ("arturo2@mail.com");
+-- INSERT INTO email_user (user_id, email_id) VALUES (1, 3);
+
+-- UPDATE email SET isVerified = true WHERE email = "daniel@mail.com"
+-- UPDATE email SET isValidated = true WHERE email = "daniel@mail.com";
