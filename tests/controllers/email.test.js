ab2e3c443cfc5a84d5d7e60fbd1f18b98c611120
Daniel Espinosa <d111092.de@gmail.com>
2023-12-12 13:18:37 -0600

    test: update verifyEmail test

--- tests/controllers/email.test.js
+++ tests/controllers/email.test.js
@@ -1,20 +1,30 @@
-const request = require('supertest');
-const expect = require('chai').expect;
+const { verifyEmail } = require('../../controllers/email.controller');
+const mysql = require('mysql2');
 
-describe('email', () => {
-    it('should verify email', (done) => {
-        request('http://localhost:8080')
-            .get('/api/email/verifyEmail')
-            .query({ email: 'daniel@mail.com' })
-            .set('Accept', 'application/json')
-			.set('Content-Type', 'application/json')
-			.end(function (err, res) {
-				
-                expect(res.statusCode).to.be.equal(200);
-				expect(res.body.msg).to.be.equal('email verified');
-				expect(res.body.email).to.be.equal('daniel@mail.com');
+jest.mock('mysql2', () => ({
+    createPool: jest.fn(),
+}));
 
-                done();
-			});
-    })
-})
+jest.mock('../../models/db', () => {
+    const mockDbConnection = {
+        execute: jest.fn(),
+    };
+
+    return jest.fn().mockImplementation(() => ({
+        setConnection: jest.fn().mockReturnValue(mockDbConnection),
+    }));
+});
+
+test('verifyEmail updates email verification status', async () => {
+    const req = { query: { email: 'test@example.com' } };
+    const res = {
+        json: jest.fn(),
+    };
+
+    await verifyEmail(req, res);
+
+    expect(res.json).toHaveBeenCalledWith({
+        msg: 'email verified',
+        email: 'test@example.com',
+    });
+});
