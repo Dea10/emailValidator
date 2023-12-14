9932b6de4d0d77a00e0f7ecdbddef8d3f55d4b4c
Daniel Espinosa <d111092.de@gmail.com>
2023-12-14 14:35:31 -0600

    feat: add register validation feat: add sp for user register

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -25,45 +25,48 @@ const getUsers = async (req, res) => {
 
 const registerUser = async (req, res) => {
     const { name, email, phone } = req.body;
+    const query = `CALL sp_register_user("${name}", "${email}", "${phone}")`;
 
-    const userInsertQry = `INSERT INTO user (name) VALUES ("${name}");`;
-    const emailInsertQry = `INSERT INTO email (email) VALUES ("${email}")`;
-    const phoneInsertQry = `INSERT INTO phone (phone) VALUES ("${phone}")`;
-
-    // insert user
-    const [{ insertId: userId }] = await dbConnection.execute(userInsertQry);
-
-    // insert email
-    const [{ insertId: emailId }] = await dbConnection.execute(emailInsertQry);
-
-    // phone
-    const [{ insertId: phoneId }] = await dbConnection.execute(phoneInsertQry);
-
-    const emailUserInsertQuery = `INSERT INTO email_user (user_id, email_id) VALUES (${userId}, ${emailId})`;
-    const phoneUserInsertQuery = `INSERT INTO phone_user (user_id, phone_id) VALUES (${userId}, ${phoneId})`;
-
-    // email_user 
-    await dbConnection.execute(emailUserInsertQuery);
-
-    // phone_user
-    await dbConnection.execute(phoneUserInsertQuery);
-
-    res.json({
-        msg: 'post API - controller',
-        newUser: {
-            id: userId,
+    if(name.length === 0 || email.length === 0 || phone.length === 0) {
+        res.status(400).json({
+            msg: "not valid info",
+            success: false,
+            name,
             email,
             phone
-        },
-        newMail: {
-            id: emailId,
-            email
-        },
-        newPhone: {
-            id: phoneId,
-            phone
-        }
-    });
+        });
+
+        return;
+    }
+
+    try {
+        await dbConnection.execute(query);
+
+        res.json({
+            msg: 'post API - controller',
+            success: true,
+            newUser: {
+                id: userId,
+                email,
+                phone
+            },
+            newMail: {
+                id: emailId,
+                email
+            },
+            newPhone: {
+                id: phoneId,
+                phone
+            }
+        });
+        
+    } catch (error) {
+        res.status(500).json({
+            msg: 'something went wrong',
+            error: error.sqlMessage,
+            success: false
+        })
+    }
 }
 
 module.exports = {
--- emailValidatorDB.sql
+++ emailValidatorDB.sql
@@ -5,18 +5,18 @@ USE email_validator;
 
 CREATE TABLE user (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255)
+    name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE email (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    email VARCHAR(255),
+    email VARCHAR(255) NOT NULL UNIQUE,
     isVerified BOOLEAN NOT NULL DEFAULT false
 );
 
 CREATE TABLE phone (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    phone VARCHAR(255),
+    phone VARCHAR(255) NOT NULL UNIQUE,
     isVerified BOOLEAN NOT NULL DEFAULT false
 );
 
@@ -35,3 +35,58 @@ CREATE TABLE phone_user (
     FOREIGN KEY (user_id) REFERENCES user(id),
     FOREIGN KEY (phone_id) REFERENCES phone(id)
 );
+
+
+-- SPs
+DELIMITER //
+
+CREATE PROCEDURE sp_register_user(
+    IN userName VARCHAR(255), 
+    IN userEmail VARCHAR(255), 
+    IN userPhone VARCHAR(255)
+)
+BEGIN
+    DECLARE last_user_id INT;
+    DECLARE last_email_id INT;
+    DECLARE last_phone_id INT;
+
+    -- Declare an exit handler for SQL exceptions
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        -- Rollback changes on exception
+        ROLLBACK;
+
+        -- Return an error message
+        SELECT 'Error: Registration failed; changes rolled back' AS error_message;
+    END;
+
+    -- Main logic
+    BEGIN
+        -- Start a transaction
+        START TRANSACTION;
+
+        -- Insert into user table
+        INSERT INTO user (name) VALUES(userName);
+        SET last_user_id = LAST_INSERT_ID();
+
+        -- Insert into email table
+        INSERT INTO email (email) VALUES(userEmail);
+        SET last_email_id = LAST_INSERT_ID();
+
+        -- Insert into phone table
+        INSERT INTO phone (phone) VALUES(userPhone);
+        SET last_phone_id = LAST_INSERT_ID();
+
+        -- Insert into email_user and phone_user tables
+        INSERT INTO email_user (user_id, email_id) VALUES(last_user_id, last_email_id);
+        INSERT INTO phone_user (user_id, phone_id) VALUES(last_user_id, last_phone_id);
+
+        -- Commit the transaction
+        COMMIT;
+
+        -- Return a success message
+        SELECT 'Registration successful' AS success_message;
+    END;
+END//
+
+DELIMITER ;
